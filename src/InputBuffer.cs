d7154c5d7125bfb694f1943c39c27102a6982931
ZeroICQ <zeroicq@gmail.com>
2018-11-03 19:50:40 +1000

    Buffer rectract

--- src/InputBuffer.cs
+++ src/InputBuffer.cs
@@ -1,19 +1,68 @@
-using System;
 using System.IO;
+using System.Text;
 
 namespace Compiler {
 
 public class InputBuffer {
     public int Column { get; protected set; } = 1;
     public int Line { get; protected set; } = 1;
+
+    public int LexemeBeginLine { get; protected set; } = 1;
+    public int LexemeBeginColumn { get; protected set; } = 1;
     
+    public string Lexeme => _buffer.ToString();
+
     private readonly TextReader _textReader;
+    private readonly StringBuilder _buffer;
+    
+    // retraction simulation
+    private bool _isRetracted = false;
+    private int lastSymbol = -1;
+    private int _prevLineColumn = 1;
+
+    public void StartLexeme() {
+        SkipWhitespaces();
+        
+        LexemeBeginLine = Line;
+        LexemeBeginColumn = Column;
+
+        _buffer.Clear();
+    }
+
+    public void Retract() {
+        _isRetracted = true;
+
+        switch (lastSymbol) {
+            case '\n':
+                Column = _prevLineColumn;
+                Line = Line > 1 ? Line - 1 : Line;
+                break;
+            case '\r':
+            case -1:
+                //do nothing
+                break;
+            default:
+                Column = Column > 1 ? Column - 1 : Column;
+                break;
+        }
+
+        //remove last symbol from lexeme buffer
+        if (_buffer.Length > 0) {
+            _buffer.Remove(_buffer.Length - 1, 1);
+        }
+    }
 
     public int Read() {
-        var symbol = _textReader.Read();
+        var symbol = ReadNext();
+        lastSymbol = symbol;
+        
+        if (symbol != -1)
+            _buffer.Append((char) symbol);
         
         switch (symbol) {
             case '\n':
+                _prevLineColumn = Column;
+                
                 Line += 1;
                 Column = 1;
                 break;
@@ -31,16 +80,12 @@ public class InputBuffer {
     }
 
     public int Peek() {
-        return _textReader.Peek();
-    }
-    
-    public InputBuffer(TextReader textReader) {
-        _textReader = textReader;
+        return _isRetracted ? lastSymbol : _textReader.Peek();
     }
 
     public void SkipLine() {
         while (true) {
-            var symbol = _textReader.Read();
+            var symbol = ReadNext();
 
             switch (symbol) {
                 case '\n':
@@ -58,19 +103,21 @@ public class InputBuffer {
         }
             
     }
-    
-// TODO: remove
+
+    public InputBuffer(TextReader textReader) {
+        _textReader = textReader;
+    }
+
     private void SkipWhitespaces() {
         while (true) {
-            var peek = _textReader.Peek();
+            var peek = Peek();
 
             switch (peek) {
+                case '\t':
                 case ' ':
                     Column += 1;
                     _textReader.Read();
                     break;
-                case '\t':
-                    goto case ' ';
                 case '\r':
                     _textReader.Read();
                     break;
@@ -82,6 +129,13 @@ public class InputBuffer {
                     return;
             }
         }
+    }
+
+    private int ReadNext() {
+        if (!_isRetracted) return _textReader.Read();
+        
+        _isRetracted = false;
+        return lastSymbol;
     }
 }
 
