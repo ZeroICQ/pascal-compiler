c7bfe80ffadd0f076d7e20a30c26094d957900a8
ZeroICQ <zeroicq@gmail.com>
2018-11-05 12:08:04 +1000

    Improve Tester output

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -18,7 +18,8 @@ class Tester {
             ShowUsage();
             return;
         }
-            
+
+        var defaultForegroundColor = Console.ForegroundColor;
         
         var testDir = Directory.GetFiles(args[1]);
         var compilerPath = args[0];
@@ -28,22 +29,20 @@ class Tester {
                 continue;
 
             var testName = testFile.Substring(0, testFile.IndexOf('.'));
-//            var checkFilename = test.Substring(test.IndexOf(".") + 1);
             
-            Process pr = new Process();
+            var pr = new Process();
             pr.StartInfo.FileName = "dotnet";
             pr.StartInfo.Arguments = $"{compilerPath} -L {testFile}";
             pr.StartInfo.UseShellExecute = false;
             pr.StartInfo.RedirectStandardOutput = true;
             pr.Start();
 
+            var foundError = false;
             using (var answer = File.OpenText(testName + ".test")) {
 
                 while (!pr.StandardOutput.EndOfStream) {
-                    if (answer.EndOfStream) {
-                        Console.WriteLine("line count mismatch in " + testName);
-                        return;
-                    }
+                    if (answer.EndOfStream)
+                        break;
                         
                     var compiledResult = pr.StandardOutput.ReadLine().Split().Where(i => i != "" && i != "\t");
                     var outLine = string.Join(" ", compiledResult);
@@ -52,10 +51,14 @@ class Tester {
                     var answerLine = string.Join(" ", expectedAnswer);
 
                     if (!compiledResult.SequenceEqual(expectedAnswer)) {
-
+                        foundError = true;
                         var diffBuilder = new InlineDiffBuilder(new Differ());
                         var diff = diffBuilder.BuildDiffModel(answerLine, outLine);
-                        Console.WriteLine($"Error in {testName}");
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"[!] Error in {testName}");
+                        Console.ForegroundColor = defaultForegroundColor;
+                        
                         foreach (var line in diff.Lines) {
                             switch (line.Type) {
                                 case ChangeType.Inserted:
@@ -73,17 +76,34 @@ class Tester {
                             }
 
                             Console.WriteLine(line.Text);
+                            Console.ForegroundColor = defaultForegroundColor;
                         }
 
 //                        Console.ForegroundColor = ConsoleColor.Black;
                         break;
                     }
-
                 }
+                
+                if(foundError)
+                    break;
+                
+                if (!answer.EndOfStream || !pr.StandardOutput.EndOfStream) {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("[-] ");
+                    Console.WriteLine($"line count mismatch in {testName}");
+                    Console.ForegroundColor = defaultForegroundColor;
+                }
+                else {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("[+] ");
+                    Console.WriteLine($"passed {testName}");
+                    Console.ForegroundColor = defaultForegroundColor;
+                }
+                
             }
             pr.WaitForExit();// Waits here for the process to exit.
             
-            Console.WriteLine($"test {testName} passed");
+            
         }
     }
 }
