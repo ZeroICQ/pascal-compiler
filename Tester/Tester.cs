f84e6fc42fd6884724b2472bbc17e54889d049bb
ZeroICQ <zeroicq@gmail.com>
2018-11-24 17:58:53 +1000

    Update Tester with new CLI parameters

--- Tester/Tester.cs
+++ Tester/Tester.cs
@@ -3,92 +3,133 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using CommandLineParser.Arguments;
+using CommandLineParser.Exceptions;
+using CommandLineParser.Validation;
 using DiffPlex;
 using DiffPlex.DiffBuilder;
 using DiffPlex.DiffBuilder.Model;
 
 namespace Tester {
 class Tester {
-    private static void ShowUsage() {
-        Console.WriteLine("USAGE");
-        Console.WriteLine("dotnet Tester.dll [pathToCompiler] [pathToTestsRoot]");
-    }
         
     static void Main(string[] args) {
-        if (args.Length < 2) {
-            ShowUsage();
+        var commandLineParser = new CommandLineParser.CommandLineParser();
+        
+        var compilerPath = new FileArgument('c', "compiler", "Path to compiler") {
+            Optional = false
+        };
+        
+        var testsDirectory = new DirectoryArgument('t', "test-dir", "Path to tests directory") {
+            Optional = false
+        };
+        
+        var testLexer = new SwitchArgument('l', "lexer", "Test lexer", false);
+        var testParser = new SwitchArgument('p', "parser", "Test parser", false);
+        var testAll = new SwitchArgument('a', "all", "Launch all tests", false);
+        
+        commandLineParser.Arguments.Add(compilerPath);
+        commandLineParser.Arguments.Add(testsDirectory);
+        
+        commandLineParser.Arguments.Add(testAll);
+        commandLineParser.Arguments.Add(testLexer);
+        commandLineParser.Arguments.Add(testParser);
+
+        var testGroupCertification = new ArgumentGroupCertification("a,p,l", EArgumentGroupCondition.AtLeastOneUsed);
+        commandLineParser.Certifications.Add(testGroupCertification);
+
+        try {
+            commandLineParser.ParseCommandLine(args);
+        }
+        catch (CommandLineException e) {
+            commandLineParser.ShowUsageHeader = "dotnet Tester.dll [pathToCompiler] [pathToTestsRoot]";
+            commandLineParser.ShowUsage();
+        }
+
+        if (testAll.Value) {
+            TestLexer(compilerPath.Value.FullName, testsDirectory.Value.FullName);
+            TestParser(compilerPath.Value.FullName, testsDirectory.Value.FullName);
             return;
         }
-        
-//        Test(args[0], $"{args[1]}\\tests\\lexer", "-l");
-        TestPrecise(args[0], $"{args[1]}\\tests\\parser", "-s");
-//        Test(args[0], $"{args[1]}\\tests\\lexer", "-l");
-        
+
+        if (testLexer.Value) 
+            TestLexer(compilerPath.Value.FullName, testsDirectory.Value.FullName);
+
+        if (testParser.Value)
+            TestParser(compilerPath.Value.FullName, testsDirectory.Value.FullName);
     }
 
-    static void Test(string compilerPath, string testDir, string flags) {
+    private static Process RunCompiler(string path, string flags) {
+        var pr = new Process();
+        
+        pr.StartInfo.FileName = "dotnet";
+        pr.StartInfo.Arguments = $"{path} {flags}";
+        pr.StartInfo.UseShellExecute = false;
+        pr.StartInfo.RedirectStandardOutput = true;
+        pr.Start();
+        
+        return pr;
+    }
+
+    private static void PrintDiff(string expectedLine, string gotLine, string testName) {
+        var diffBuilder = new InlineDiffBuilder(new Differ());
+        var diff = diffBuilder.BuildDiffModel(expectedLine, gotLine);
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"[!] Error in {testName}");
+                        
+        foreach (var line in diff.Lines) {
+            switch (line.Type) {
+                case ChangeType.Inserted:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("+ ");
+                    break;
+                case ChangeType.Deleted:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("- ");
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("  ");
+                    break;
+            }
+
+            Console.WriteLine(line.Text);
+                            
+        }
+    }
+
+    private static void TestLexer(string compilerPath, string testDir) {
         var defaultForegroundColor = Console.ForegroundColor;
         
-        var testFiles = Directory.GetFiles(testDir);
+        var testFiles = Directory.GetFiles($"{testDir}/lexer/");
 
         foreach (var testFile in testFiles) {
             if (!testFile.EndsWith(".pas"))
                 continue;
 
             var testName = testFile.Substring(0, testFile.LastIndexOf('.'));
-            
-            var pr = new Process();
-            pr.StartInfo.FileName = "dotnet";
-            pr.StartInfo.Arguments = $"{compilerPath} {flags} {testFile}";
-            pr.StartInfo.UseShellExecute = false;
-            pr.StartInfo.RedirectStandardOutput = true;
-            pr.Start();
-
+            var pr = RunCompiler(compilerPath, $"-l {testFile}");
             var foundError = false;
-            using (var answer = File.OpenText(testName + ".test")) {
+            
+            using (var answer = File.OpenText($"{testName}.test")) {
 
                 while (!pr.StandardOutput.EndOfStream) {
                     if (answer.EndOfStream)
                         break;
                         
-                    var compiledResult = pr.StandardOutput.ReadLine().Split().Where(i => i != "" && i != "\t");
-                    var outLine = string.Join(" ", compiledResult);
+                    var gotResult = pr.StandardOutput.ReadLine().Split().Where(i => i != "" && i != "\t");
+                    var gotLine = string.Join(" ", gotResult);
 
-                    var expectedAnswer = answer.ReadLine().Split().Where(i => i != "" && i != "\t");
-                    var answerLine = string.Join(" ", expectedAnswer);
+                    var expectedResult = answer.ReadLine().Split().Where(i => i != "" && i != "\t");
+                    var expectedLine = string.Join(" ", expectedResult);
 
-                    if (!compiledResult.SequenceEqual(expectedAnswer)) {
-                        foundError = true;
-                        var diffBuilder = new InlineDiffBuilder(new Differ());
-                        var diff = diffBuilder.BuildDiffModel(answerLine, outLine);
-
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"[!] Error in {testName}");
-                        Console.ForegroundColor = defaultForegroundColor;
-                        
-                        foreach (var line in diff.Lines) {
-                            switch (line.Type) {
-                                case ChangeType.Inserted:
-                                    Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.Write("+ ");
-                                    break;
-                                case ChangeType.Deleted:
-                                    Console.ForegroundColor = ConsoleColor.Green;
-                                    Console.Write("- ");
-                                    break;
-                                default:
-                                    Console.ForegroundColor = ConsoleColor.White;
-                                    Console.Write("  ");
-                                    break;
-                            }
-
-                            Console.WriteLine(line.Text);
-                            Console.ForegroundColor = defaultForegroundColor;
-                        }
-
-//                        Console.ForegroundColor = ConsoleColor.Black;
-                        break;
-                    }
+                    if (gotLine.SequenceEqual(expectedLine)) 
+                        continue;
+                    
+                    foundError = true;
+                    PrintDiff(expectedResult.ToString(), gotResult.ToString(), testName);
+                    break;
                 }
                 
                 if(foundError)
@@ -96,44 +137,35 @@ class Tester {
                 
                 if (!answer.EndOfStream || !pr.StandardOutput.EndOfStream) {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write("[-] ");
-                    Console.WriteLine($"line count mismatch in {testName}");
-                    Console.ForegroundColor = defaultForegroundColor;
+                    Console.WriteLine($"[-] line count mismatch in {testName}");
                 }
                 else {
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write("[+] ");
-                    Console.WriteLine($"passed {testName}");
-                    Console.ForegroundColor = defaultForegroundColor;
+                    Console.WriteLine($"[+] passed {testName}");
                 }
                 
+                Console.ForegroundColor = defaultForegroundColor;
             }
             pr.WaitForExit();// Waits here for the process to exit.
-            
             
         }
     }
 
-    static void TestPrecise(string compilerPath, string testDir, string flags) {
+    static void TestParser(string compilerPath, string testDir) {
         var defaultForegroundColor = Console.ForegroundColor;
         
-        var testFiles = Directory.GetFiles(testDir);
+        var testFiles = Directory.GetFiles($"{testDir}/parser");
 
         foreach (var testFile in testFiles) {
             if (!testFile.EndsWith(".pas"))
                 continue;
 
             var testName = testFile.Substring(0, testFile.LastIndexOf('.'));
+
+            var pr = RunCompiler(compilerPath, $"-s {testFile}");
             
-            var pr = new Process();
-            pr.StartInfo.FileName = "dotnet";
-            pr.StartInfo.Arguments = $"{compilerPath} {flags} {testFile}";
-            pr.StartInfo.UseShellExecute = false;
-            pr.StartInfo.RedirectStandardOutput = true;
-            pr.StartInfo.StandardOutputEncoding = Encoding.UTF8;
-            pr.Start();
             var foundError = false;
-            using (var answer = File.OpenText(testName + ".test")) {
+            using (var answer = File.OpenText($"{testName}.test")) {
                 
                 while (!pr.StandardOutput.EndOfStream) {
                     if (answer.EndOfStream)
@@ -142,38 +174,12 @@ class Tester {
                     var outLine = pr.StandardOutput.ReadLine();
                     var answerLine = answer.ReadLine();
 
-                    if (!outLine.Equals(answerLine)) {
-                        foundError = true;
-                        var diffBuilder = new InlineDiffBuilder(new Differ());
-                        var diff = diffBuilder.BuildDiffModel(answerLine, outLine);
-
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"[!] Error in {testName}");
-                        Console.ForegroundColor = defaultForegroundColor;
-                        
-                        foreach (var line in diff.Lines) {
-                            switch (line.Type) {
-                                case ChangeType.Inserted:
-                                    Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.Write("+ ");
-                                    break;
-                                case ChangeType.Deleted:
-                                    Console.ForegroundColor = ConsoleColor.Green;
-                                    Console.Write("- ");
-                                    break;
-                                default:
-                                    Console.ForegroundColor = ConsoleColor.White;
-                                    Console.Write("  ");
-                                    break;
-                            }
-
-                            Console.WriteLine(line.Text);
-                            Console.ForegroundColor = defaultForegroundColor;
-                        }
-
-//                        Console.ForegroundColor = ConsoleColor.Black;
-                        break;
-                    }
+                    if (outLine.Equals(answerLine)) 
+                        continue;
+                    
+                    foundError = true;
+                    PrintDiff(answerLine, outLine, testName);
+                    break;
                 }
                 
                 if(foundError)
@@ -181,17 +187,14 @@ class Tester {
                 
                 if (!answer.EndOfStream || !pr.StandardOutput.EndOfStream) {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write("[-] ");
-                    Console.WriteLine($"line count mismatch in {testName}");
-                    Console.ForegroundColor = defaultForegroundColor;
+                    Console.WriteLine($"[-] line count mismatch in {testName}");
                 }
                 else {
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write("[+] ");
-                    Console.WriteLine($"passed {testName}");
-                    Console.ForegroundColor = defaultForegroundColor;
+                    Console.WriteLine($"[+] passed {testName}");
                 }
                 
+                Console.ForegroundColor = defaultForegroundColor;
             }
             pr.WaitForExit();// Waits here for the process to exit.
             
