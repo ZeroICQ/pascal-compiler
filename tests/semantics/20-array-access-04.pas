e935261f5c8c0940aba9e0d5bbc106bc797c7249
ZeroICQ <zeroicq@gmail.com>
2018-12-20 20:35:29 +1000

    Array access tests

---
+++ tests/semantics/20-array-access-04.pas
@@ -0,0 +1,8 @@
+var 
+    a : Array[1..100] of integer;
+    b : float;
+begin
+    
+    b := a[20*2 - 7];
+end. 
+    
---
+++ tests/semantics/20-array-access-05.pas
@@ -0,0 +1,8 @@
+var 
+    a : Array[1..100] of float;
+    b : float;
+begin
+    
+    b := a[20*2 - 7];
+end. 
+    
